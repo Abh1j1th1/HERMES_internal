1b3a98bef367519940de294e98f3bed11af2354c
jaykawade <jaykawade06@gmail.com>
2026-03-27 21:29:55 +0530

    Route root path based on auth state

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { RoleGuard } from './RoleGuard'
 
+import Home from '../pages/Home'
 import Login from '../pages/Login'
 import Signup from '../pages/Signup'
 import Unauthorized from '../pages/Unauthorized'
@@ -24,6 +25,7 @@ import DoctorAvailability from '../pages/doctor/Availability'
 import DoctorProfile from '../pages/doctor/Profile'
 
 export const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
   { path: '/login', element: <Login /> },
   { path: '/signup', element: <Signup /> },
   { path: '/unauthorized', element: <Unauthorized /> },
